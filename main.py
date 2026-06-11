7c6ae33313e59e5ec06b4b3f6a3b709c7bf84e39
V <varunshankar2k6@gmail.com>
2026-06-11 16:01:15 +0530

    Main class with response

--- main.py
+++ main.py
@@ -1,22 +1,54 @@
 from fastapi import FastAPI
-from models.student import Student
-
+from models.student import Student, StudentResponse
 app = FastAPI()
-
-
 @app.get("/greet")
 def greet(name: str):
     return {"message": f"Hello {name}"}
 
-
-@app.post("/student")
+#Post
+@app.post("/student", response_model=StudentResponse)
 def create_student(student: Student):
-    return student
-
-
-@app.post("/student/{course_id}")
-def create_student_with_id(course_id: int, student: Student):
     return {
-        "course_id": course_id,
-        "student": student
+        "id": 101,
+        **student.model_dump()
+    }
+
+#Get
+@app.get("/student/{student_id}", response_model=StudentResponse)
+def get_student(student_id: int):
+    return {
+        "id": student_id,
+        "name": "John",
+        "age": 20,
+        "course": "Python",
+        "email": "john@gmail.com",
+        "address": {
+            "city": "Trivandrum",
+            "state": "Kerala"
+        },
+        "courses": [
+            "Python",
+            "FastAPI"
+        ]
+    }
+
+
+# PUT
+@app.put("/student/{student_id}")
+def update_student(student_id: int, student: Student):
+    return {
+        "message": "Student updated successfully",
+        "student": {
+            "id": student_id,
+            **student.model_dump()
+        }
+    }
+
+
+# DELETE
+@app.delete("/student/{student_id}")
+def delete_student(student_id: int):
+    return {
+        "message": "Student deleted successfully",
+        "student_id": student_id
     }

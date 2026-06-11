67c88c60c48fa406dcfed888758634d491fedee7
V <varunshankar2k6@gmail.com>
2026-06-11 16:00:53 +0530

    Student with response

--- models/student.py
+++ models/student.py
@@ -1,14 +1,19 @@
-from pydantic import BaseModel, Field, EmailStr
+from pydantic import BaseModel, EmailStr
 from typing import Optional
-
 class Address(BaseModel):
     city: str
     state: str
-
-
 class Student(BaseModel):
-    name: str = Field(min_length=3, max_length=50)
-    age: int = Field(gt=18, lt=100)
+    name: str
+    age: int
+    course: str
+    email: Optional[EmailStr] = None
+    address: Address
+    courses: list[str]
+class StudentResponse(BaseModel):
+    id: int
+    name: str
+    age: int
     course: str
     email: Optional[EmailStr] = None
     address: Address

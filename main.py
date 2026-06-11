d970b8ced78763c4675853af10647deb29a90bd0
V <varunshankar2k6@gmail.com>
2026-06-11 13:01:17 +0530

    Main code

--- main.py
+++ main.py
@@ -1,29 +1,19 @@
 from fastapi import FastAPI
-from pydantic import BaseModel, Field, EmailStr
-from typing import Optional
+from models.student import Student
+
 app = FastAPI()
+
+
 @app.get("/greet")
 def greet(name: str):
     return {"message": f"Hello {name}"}
+
+
 @app.post("/student")
-def create_student(student: dict):
-    return {
-        "name": student["name"],
-        "course": student["course"]
-    }
-class Address(BaseModel):
-    city: str
-    state: str
-class Student(BaseModel):
-    name: str = Field(min_length=3, max_length=50)
-    age: int = Field(gt=18, lt=100)
-    course: str
-    email: Optional[EmailStr] = None
-    address: Address
-    courses: list[str]
-@app.post("/students")
-def create_students(student: Student):
+def create_student(student: Student):
     return student
+
+
 @app.post("/student/{course_id}")
 def create_student_with_id(course_id: int, student: Student):
     return {
